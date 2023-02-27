1f962eab455cdb84934de07d7dd9ee7d962fca33
m0ntz <109100764+m0ntz@users.noreply.github.com>
2023-02-27 12:06:08 +0100

    imported axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  let data = {
+  const [city, setCity] = useState(null);
+  const [data, setData] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function searchCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showTemperature(response) {
+    setData(true);
+
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiKey = "117ab8d8bcf8c7c5acd0f501978c9bbf";
+    let unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(showTemperature);
+  }
+
+  let form = (
+    <form id="search-form" onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="col-9">
+          <input
+            type="search"
+            placeholder="Enter location"
+            class="form-control"
+            id="city-input"
+            onChange={searchCity}
+          />
+        </div>
+        <div className="col-3">
+          <input type="submit" value="Search" className="btn btn-primary" />
+        </div>
+      </div>
+    </form>
+  );
+
+  let staticData = {
     city: "Düsseldorf",
     date: "Sunday 14:00",
     description: "cloudy",
@@ -12,72 +61,119 @@ export default function Weather() {
     wind: 5,
   };
 
-  return (
-    <body>
-      <div class="container">
-        <div class="weather-app">
-          <form id="search-form">
-            <div class="row">
-              <div class="col-9">
-                <input
-                  type="search"
-                  placeholder="Enter location"
-                  class="form-control"
-                  id="city-input"
-                />
-              </div>
-              <div class="col-3">
-                <input type="submit" value="Search" class="btn btn-primary" />
-              </div>
-            </div>
-          </form>
-          <div class="overview">
-            <h1 id="city">{data.city}</h1>
-            <ul>
-              <li>
-                Last updated: {data.date} <span id="date"></span>
-              </li>
-              <li id="description">{data.description}</li>
-            </ul>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <div class="d-flex weather-temperature">
-                <img
-                  src={data.icon}
-                  alt="rain-icon"
-                  class="float-left"
-                  id="icon"
-                />
-                <strong id="temperature">{data.temperature}</strong>
-                <span class="units">°C</span>
-              </div>
-            </div>
-            <div class="col-6">
+  if (data) {
+    return (
+      <div className="body">
+        <div className="container">
+          <div className="weather-app">
+            {form}
+            <div className="overview">
+              <h1 id="city" style={{ textTransform: "capitalize" }}>
+                {city}
+              </h1>
               <ul>
                 <li>
-                  Humidity: {data.humidity}
-                  <span id="humidity"></span>%
+                  Last updated: Today <span id="date"></span>
                 </li>
-                <li>
-                  Wind: {data.wind} <span id="wind"></span> km/h
-                </li>
+                <li id="description">{weather.description}</li>
               </ul>
             </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="d-flex weather-temperature">
+                  <img
+                    src={weather.icon}
+                    alt="Weather Icon"
+                    className="float-left"
+                    id="icon"
+                  />
+                  <strong id="temperature">{weather.temperature}</strong>
+                  <span className="units">°C</span>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: {weather.humidity}
+                    <span id="humidity"></span>%
+                  </li>
+                  <li>
+                    Wind: {weather.wind} <span id="wind"></span> km/h
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="weather-forecast" id="forecast"></div>
           </div>
-          <div class="weather-forecast" id="forecast"></div>
+          <footer>
+            <a
+              href="https://github.com/m0ntz/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by Montserrat Chavez
+          </footer>
         </div>
-        <footer>
-          <a
-            href="https://github.com/m0ntz/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by Montserrat Chavez
-        </footer>
       </div>
-    </body>
-  );
+    );
+  } else {
+    return (
+      <div className="body">
+        <div className="container">
+          <div className="weather-app">
+            {form}
+            <div className="overview">
+              <h1 id="city" style={{ textTransform: "capitalize" }}>
+                {staticData.city}
+              </h1>
+              <ul>
+                <li>
+                  Last updated: Today <span id="date"></span>
+                </li>
+                <li id="description">{weather.description}</li>
+              </ul>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className="d-flex weather-temperature">
+                  <img
+                    src={staticData.icon}
+                    alt="Weather Icon"
+                    className="float-left"
+                    id="icon"
+                  />
+                  <strong id="temperature">{staticData.temperature}</strong>
+                  <span className="units">°C</span>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Humidity: {staticData.humidity}
+                    <span id="humidity"></span>%
+                  </li>
+                  <li>
+                    Wind: {staticData.wind} <span id="wind"></span> km/h
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="weather-forecast" id="forecast"></div>
+          </div>
+          <footer>
+            <a
+              href="https://github.com/m0ntz/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by Montserrat Chavez
+          </footer>
+        </div>
+      </div>
+    );
+  }
 }
